3a516043e25d78f99a09a3951c628a7e1dbda311
Jack Gitter <gitter.j@husky.neu.edu>
2021-01-14 13:36:25 -0500

    made it write to a file

--- xmlparsermodel.py
+++ xmlparsermodel.py
@@ -6,22 +6,24 @@ class Model():
 		self.XMLFilePath = XMLFilePath
 		self.tree = ET.parse(open(self.XMLFilePath))
 		self.root = self.tree.getroot()
+		self.file = open(self.XMLFilePath[0:len(self.XMLFilePath)-4] + "_PARSED.txt", "w")
 
 	def parseXMLHelper(self):
 		self.parseXML(self.root, [], 0)
 
 	def parseXML(self, root, seenElems, tabCounter):
  
+
 		if len(list(root)) == 0:
 			for i in range(0, tabCounter):
-				print("\t", end="")
-			print(root.tag + ": " + root.text + "\n")
+				self.file.write("\t")
+			self.file.write(root.tag + ": " + root.text + "\n")
 		else:
 			for i in range(0, tabCounter):
-				print("\t", end="")
+				self.file.write("\t")
 			for i in range(0, len((list(root)))):
 				if (root not in seenElems):
-					print(root.tag + ": " + root.text)
+					self.file.write(root.tag + ": " + root.text)
 					tabCounter+=1
 					seenElems.append(root)
 				self.parseXML(list(root)[i], seenElems, tabCounter)
